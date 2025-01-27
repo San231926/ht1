122ada7dce7384fac6b56fe2a4c7a946c9b0fa92
Diego Antonio De León Enríquez <dele24247@uvg.edu.gt>
2025-01-26 18:42:15 -0600

    Update LicuadoraTest.java
    Se implementan distintas pruebas:
    - verificar que la licuadora se enciende cuando tiene contenido
    - evitar encender sin contenido
    - llenar más que la capacidad, verifica que la licuadora puede ser llenada y que detecta cuando esté llena
    - incrementar la velocidad

--- licuadora-app/src/test/java/com/licuadora/LicuadoraTest.java
+++ licuadora-app/src/test/java/com/licuadora/LicuadoraTest.java
@@ -12,4 +12,26 @@ class LicuadoraTest {
   void setUp() {
     this.licuadora = new LicuadoraImpl(2.0);
   }
-  
+  @Test
+  void testEncenderConContenido() {
+    this.licuadora.llenar(1.0);
+    this.licuadora.encender();
+    Assertions.assertTrue(this.licuadora.estaEncendida(), "La licuadora debería estar encendida");
+  }
+  @Test
+  void testEncenderSinContenido() {
+    this.licuadora.encender();
+    Assertions.assertFalse(this.licuadora.estaEncendida(); "La licuadora no debería encenderse sin contenido");
+  }
+  @Test
+  void testLlenarExcedeCapacidad() {
+    this.licuadora.llenar(3.0);
+    Assertions.assertTrue(this.licuadora.estaLlena(), "La licuadora debería estar llena");
+  }
+  @Test
+  void testIncrementarVelocidad() {
+    this.licuadora.llenar(1.0);
+    this.licuadora.encender();
+    this.licuadora.incrementarVelocidad();
+    Assertions.assertEquals(2, this.licuadora.consultarVelocidad(), "La velocidad debería ser 2");
+  }
